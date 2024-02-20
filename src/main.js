6eb5f35cf58513f2cfa867c9879e8025df8a0e68
Mila <46803466+mila1634@users.noreply.github.com>
2024-02-20 13:36:39 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/style.css'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+app.use(Toast);
